3c3926ba846ac3957e0882436e18d5a98b58b321
TPHRyan <ryan.paroz@gmail.com>
2021-12-02 02:27:37 +1000

    Implement basic verb/file arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    let program_name = args.next().unwrap();
+    match parse_args(args) {
+        Ok(app_params) => {
+            println!("Success!");
+            println!("Verb: {}", app_params.verb);
+            println!("Data file: {}", app_params.data_file_path);
+        },
+        Err(message) => {
+            println!("{}", message);
+            println!("Usage: {} <data-file-path>", program_name)
+        }
+    }
+}
+
+fn parse_args(mut args: env::Args) -> Result<AppParams, String> {
+    let verb = match args.next() {
+        Some(arg) => arg,
+        None => return Err("Verb not provided!".to_string())
+    };
+    let data_file_path = match args.next() {
+        Some(arg) => arg,
+        None => return Err("Data file path not provided!".to_string())
+    };
+    let data_file = match fs::File::open(&data_file_path) {
+        Ok(file) => file,
+        Err(e) => return Err(e.to_string())
+    };
+    Ok(AppParams {
+        verb,
+        data_file_path,
+        data_file
+    })
+}
+
+struct AppParams {
+    verb: String,
+    data_file_path: String,
+    data_file: fs::File,
 }
