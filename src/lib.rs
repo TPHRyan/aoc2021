2f2fcf071d6d7bddc7cafd006290c04826289f66
TPHRyan <ryan.paroz@gmail.com>
2021-12-03 14:26:58 +1000

    Refactor to better split responsibilities

--- src/lib.rs
+++ src/lib.rs
@@ -2,27 +2,15 @@ mod common;
 mod verbs;
 
 use std::env;
-use std::fmt::Write;
 use std::fs::File;
 
 use common::AppParams;
 
-pub fn run(mut args: env::Args) -> Result<(), String> {
-    let program_name = args.next().unwrap();
-    let app_params = match parse_args(args) {
-        Ok(params) => params,
-        Err(message) => {
-            let mut full_message = message.clone();
-            return match writeln!(full_message, "Usage: {} <data-file-path>", program_name) {
-                Ok(()) => Err(full_message),
-                Err(err) => Err(format!("{}", err)),
-            };
-        }
-    };
+pub fn run(app_params: AppParams) -> Result<(), String> {
     return verbs::call_verb(app_params);
 }
 
-fn parse_args(mut args: env::Args) -> Result<AppParams, String> {
+pub fn parse_args(mut args: env::Args) -> Result<AppParams, String> {
     let verb = match args.next() {
         Some(arg) => arg,
         None => return Err(String::from("Verb not provided!")),
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
 fn main() {
-    if let Err(message) = aoc2021::run(std::env::args()) {
-        eprintln!("{}", message);
-    }
+    let mut args = std::env::args();
+    let program_name = args.next().unwrap();
+    match aoc2021::parse_args(args) {
+        Ok(app_params) => {
+            if let Err(message) = aoc2021::run(app_params) {
+                eprintln!("{}", message);
+            }
+        }
+        Err(message) => {
+            eprintln!("{}\nUsage: {} [options]", message, program_name);
+        }
+    };
 }
