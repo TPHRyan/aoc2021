ec2dfa4bcd436d7e4b7076342c271310ebe88ddc
TPHRyan <ryan.paroz@gmail.com>
2021-12-08 01:02:34 +1000

    Implement running the bingo games and showing winner

--- src/sub/bingo.rs
+++ src/sub/bingo.rs
@@ -1,14 +1,40 @@
 mod bingo;
 pub mod board;
 
+use crate::sub::bingo::board::BingoBoard;
 use crate::Result;
 
 pub fn run_bingo(random_numbers: Vec<u32>, board_inputs: Vec<String>) -> Result<()> {
-    println!("Random numbers: {:?}", random_numbers);
-    println!("Boards:");
-    for board_input in board_inputs {
-        let parsed_board = board::BingoBoard::from_str(&board_input)?;
-        println!("{}", parsed_board)
-    }
+    let boards_result: Result<Vec<BingoBoard>> = board_inputs
+        .iter()
+        .map(|board_input| board::BingoBoard::from_str(board_input))
+        .collect();
+    let mut boards = boards_result?;
+    let winners = play_bingo_with_boards(&random_numbers, &mut boards);
+    println!(
+        "Winning board(s): {}",
+        winners
+            .iter()
+            .map(|i| format!("{}", i + 1))
+            .collect::<Vec<String>>()
+            .join(", ")
+    );
+    boards.iter().for_each(|board| println!("{}", board));
     Ok(())
+}
+
+fn play_bingo_with_boards(random_numbers: &Vec<u32>, boards: &mut Vec<BingoBoard>) -> Vec<usize> {
+    for n in random_numbers {
+        boards.iter_mut().for_each(|board| board.call_number(*n));
+        let winning_boards: Vec<usize> = boards
+            .iter()
+            .enumerate()
+            .filter(|(i, board)| board.has_bingo())
+            .map(|(i, board)| i)
+            .collect();
+        if !winning_boards.is_empty() {
+            return winning_boards;
+        }
+    }
+    vec![]
 }
